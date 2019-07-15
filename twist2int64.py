6ae2e20cbe8e4fee60c0d6db417dec28a7242834
Ryoma Kawakami <ryomakwkm@gmail.com>
2019-07-15 01:58:09 +0000

    Made left motor node

---
+++ twist2int64.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Int64
+
+class Twist2int64():
+  def __init__(self):
+    self.command_left = Int64()
+    self.command_right = Int64()
+    self.received_twist = None
+    rospy.init_node('Twist2int64')
+    rospy.Subscriber('motor/twist/cmd_vel', Twist, self.callback)
+    self.pub_right = rospy.Publisher('right_motor/cmd_vel', Int64, queue_size=10)
+    self.pub_left = rospy.Publisher('left_motor/cmd_vel', Int64, queue_size=10)
+
+  def main_twist2int64(self):
+    rospy.spin()
+
+  def callback(self, message):
+    self.received_twist = message
+    self.command_right, self.command_left = self.twist2rpm(self.received_twist)
+    self.pub_right.publish(self.command_right)
+    self.pub_left.publish(self.command_left)
+
+  def twist2rpm(self, received_data):
+    wheel_size = 0.04
+    axle_length = 0.04
+
+    v = received_data.linear.x
+    omega = received_data.angular.z
+
+    v_r = (omega*axle_length + 2*v)/2
+    v_l = (omega*axle_length - 2*v)/(-2)
+
+    v_r = v_r/(wheel_size * 2 * 3.14)
+    v_l = v_l/(wheel_size * 2 * 3.14)
+
+    return v_r, v_l
+
+# Main
+Convert = Twist2int64()
+Convert.main_twist2int64()
