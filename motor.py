c125def7648906597d7b1c3878b78d29ef984a8f
Ryoma Kawakami <rkawakami@si.lan>
2019-07-17 15:32:16 -0400

    Updated object organization

--- motor.py
+++ motor.py
@@ -5,6 +5,7 @@ INPUT1 = 1
 INPUT2 = 2
 
 class Motor():
+    # Initialization
     def __init__(self, port, rev = False):
         self.pi = pigpio.pi()
         self.port = port
@@ -14,22 +15,39 @@ class Motor():
         self.pi.write(self.port[INPUT1], 0)
         self.pi.write(self.port[INPUT2], 0)
 
+    # Stop
+    def stop(self):
+        self.pi.stop()
+
+    # Set input 1 and 2
     def setOutput(self, out):
         self.pi.write(self.port[INPUT1], out[0])
         self.pi.write(self.port[INPUT2], out[1])
 
-    def setSpeed(self, d):
-        self.duty = d
+    # Set RPM
+    def setRPM(self, r):
+        self.rpm = r
+        self.duty = self.rpm2Duty(self.rpm)
+        self.setDuty(self.duty)
+        return self.duty
 
+    # Set duty
+    def setDuty(self, d):
+        # Determine direction
         if (d >= 0 and not self.rev) or (d < 0 and self.rev):
             self.setOutput([1, 0])
         else:
             self.setOutput([0, 1])
+        # Set PWM
+        self.pi.set_PWM_dutycycle(self.port[PWM], d)
 
-        self.pi.set_PWM_dutycycle(self.port[PWM], self.duty)
-
-    def stop(self):
-        self.pi.stop()
+    # Convert RPM to duty
+    def rpm2Duty(self, rpm):
+        duty = rpm * 2.55 / 2.38
+        if abs(duty) > 100:
+            if duty > 0: duty = 100
+            else: duty = -100
+        return duty
 
 if __name__ == '__main__':
     pwm = input('PWM pin: ')
