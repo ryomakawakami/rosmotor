558d46a30a4ef0da94b130fce6a0f07adf8e796d
Ryoma Kawakami <rkawakami@si.lan>
2019-07-25 10:40:29 -0400

    Added files back

---
+++ raspberrypi/old_motor/pigpioRight.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+import pigpio
+import time
+import rospy
+from std_msgs.msg import Int64
+
+class RightMotorControl():
+  def __init__(self):
+    # Initialize
+    self.dir = True
+    self.pin_pwm = 13
+    self.pin_input1 = 19
+    self.pin_input2 = 26
+
+    # Set up node
+    rospy.init_node('right_motor_test')
+    rospy.Subscriber('right_motor/cmd_vel', Int64, self.callback)
+    rospy.loginfo('Ready')
+
+    # Set up GPIO
+    pi.write(self.pin_input1, 0)
+    pi.write(self.pin_input2, 0)
+
+  def motor_main(self):
+    rospy.spin()
+    pi.stop()
+
+  def callback(self, data):
+    # Find duty
+    self.rpm = data.data
+    duty = self.rpm2duty(self.rpm)
+    rospy.loginfo('RPM: ' + str(self.rpm))
+    rospy.loginfo('Duty: ' + str(duty))
+
+    # Set up direction
+    if duty >= 0:
+      self.dir = True
+    else:
+      self.dir = False
+      duty = -duty
+
+    if self.dir:
+      pi.write(self.pin_input1, 1)
+      pi.write(self.pin_input2, 0)
+    else:
+      pi.write(self.pin_input1, 0)
+      pi.write(self.pin_input2, 1)
+
+    # Set PWM
+    rospy.loginfo('Updating motor speed')
+    pi.set_PWM_dutycycle(self.pin_pwm, duty)
+
+  # TODO: Add min speed
+  def rpm2duty(self, rpm):
+    d = rpm * 2.55 / 2.38
+    if abs(d) > 100:
+      if d > 0: d = 100
+      else: d = -100
+    return d
+
+if __name__ == '__main__':
+  pi = pigpio.pi()
+  right = RightMotorControl()
+  right.motor_main()
